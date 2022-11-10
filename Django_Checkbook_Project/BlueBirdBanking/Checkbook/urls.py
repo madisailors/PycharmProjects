22de2a9b4b6e08f545513eafb50a91af7cd1165f
madisailors <109822907+madisailors@users.noreply.github.com>
2022-11-09 18:31:12 -0800

    checkbook draft

---
+++ Django_Checkbook_Project/BlueBirdBanking/Checkbook/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    #sets the url path to home page index.html
+    path('', views.home, name='index'),
+    #sets the url path to create new account page
+    path('create/', views.create_account, name='create'),
+    path('balance/', views.balance, name='balance'),
+    path('transaction/', views.transaction, name='transaction'),
+]
